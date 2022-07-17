f432e7a12957faeec5058805c4e52bbb8812213e
simon waronja <simon.waronja@student.moringaschool.com>
2022-07-17 20:22:25 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,20 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+
+  return (
+  
+  <div id = '#about'>
+
+  <h2>About Me</h2>
+
+  <p>Content of my choosing</p>
+
+  <img src = "" alt = "i made this">  </img>
+  
+  </div>
+
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,19 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+
+
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  
+  <div id = '#home'> 
+
+  <h1 style = ""> Your {name} is a Web Developer from Your {city} </h1>
+  
+  </div>
+
+
+  );
 }
 
 export default Home;
